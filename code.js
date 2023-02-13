aa68ee2df7efe881b87006b0e5e6c4341b449f7b
Elvira Magasumova <mova.es94@gmail.com>
2023-02-13 11:03:12 +0500

    refactoring

--- code.js
+++ code.js
@@ -1,50 +1,55 @@
 const data = JSON.parse(dataProducts);
-console.log(data);
-
 const fiCatalogEl = document.querySelector(".fi_catalog");
 const templateEl = document.querySelector(".template");
 const btnViewAllEl = document.querySelector(".view_all");
 
 let end = 6;
 let start = 0;
+const visibleCount = 3;
 let counter = 0;
 
 showItems();
+
 let addToCartBtn = document.querySelectorAll(".cart");
 addItemToCart(addToCartBtn);
 
 btnViewAllEl.addEventListener("click", function (e) {
   start = end;
-  end = end + 3;
+  end = end + visibleCount;
+
   showItems();
-  if (end == data.length) {
+
+  if (end >= data.length) {
     btnViewAllEl.classList.add("hide");
   }
+
   const newBtn = document.querySelectorAll(".cart");
   addToCartBtn = newBtn;
   addItemToCart(newBtn);
 });
 
 function showItems() {
-  for (i = start; i < end; i++) {
+  const dataSlice = data.slice(start, end);
+
+  dataSlice.forEach((item) => {
     const fiCatalogItemEl = templateEl.content
       .querySelector(".fi_catalog_item")
       .cloneNode(true);
 
     const imgEl = fiCatalogItemEl.querySelector(".fi_catalog_image");
-    imgEl.src = data[i].img;
+    imgEl.src = item.img;
 
     const itemNameEl = fiCatalogItemEl.querySelector(".item_name");
-    itemNameEl.textContent = data[i].name;
+    itemNameEl.textContent = item.name;
 
     const descriptionEl = fiCatalogItemEl.querySelector(".description");
-    descriptionEl.textContent = data[i].description;
+    descriptionEl.textContent = item.description;
 
     const priceEl = fiCatalogItemEl.querySelector(".price");
-    priceEl.textContent = data[i].price;
+    priceEl.textContent = item.price;
 
     fiCatalogEl.appendChild(fiCatalogItemEl);
-  }
+  });
 }
 
 const countBlock = document.querySelector(".count_block");
@@ -60,6 +65,60 @@ function addItemToCart(buttons) {
   });
 }
 
+// let end = 6;
+// let start = 0;
+// let counter = 0;
+
+// showItems();
+// let addToCartBtn = document.querySelectorAll(".cart");
+// addItemToCart(addToCartBtn);
+
+// btnViewAllEl.addEventListener("click", function (e) {
+//   start = end;
+//   end = end + 3;
+//   showItems();
+//   if (end == data.length) {
+//     btnViewAllEl.classList.add("hide");
+//   }
+// const newBtn = document.querySelectorAll(".cart");
+// addToCartBtn = newBtn;
+// addItemToCart(newBtn);
+// });
+
+// function showItems() {
+//   for (i = start; i < end; i++) {
+//     const fiCatalogItemEl = templateEl.content
+//       .querySelector(".fi_catalog_item")
+//       .cloneNode(true);
+
+//     const imgEl = fiCatalogItemEl.querySelector(".fi_catalog_image");
+//     imgEl.src = data[i].img;
+
+//     const itemNameEl = fiCatalogItemEl.querySelector(".item_name");
+//     itemNameEl.textContent = data[i].name;
+
+//     const descriptionEl = fiCatalogItemEl.querySelector(".description");
+//     descriptionEl.textContent = data[i].description;
+
+//     const priceEl = fiCatalogItemEl.querySelector(".price");
+//     priceEl.textContent = data[i].price;
+
+//     fiCatalogEl.appendChild(fiCatalogItemEl);
+//   }
+// }
+
+// const countBlock = document.querySelector(".count_block");
+// let cartCounter = document.querySelector(".cart_num");
+
+// function addItemToCart(buttons) {
+//   buttons.forEach((button) => {
+//     button.addEventListener("click", function (e) {
+//       countBlock.classList.remove("hide");
+//       counter += 1;
+//       cartCounter.textContent = counter;
+//     });
+//   });
+// }
 
 // data.forEach(element => {
 //     const fiCatalogItemEl = templateEl.content.querySelector('.fi_catalog_item').cloneNode(true);
